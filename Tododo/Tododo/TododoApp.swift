71a56fab94e9f079192500ead29f5f4c38bbabec
SusumuSarai <susumu_sarai@mail.toyota.co.jp>
2023-06-14 11:24:56 +0900

    Retry initialize

---
+++ Tododo/Tododo/TododoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TododoApp.swift
+//  Tododo
+//
+//  Created by user on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct TododoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
