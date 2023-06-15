c494f6d7125c3aeace4d4536c5b826e32597e6f7
SusumuSarai <susumu_sarai@mail.toyota.co.jp>
2023-06-15 16:46:40 +0900

    looks OK

--- Todododo/Todododo/ContentView.swift
+++ Todododo/Todododo/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        List{
-            HStack{Text("☑︎")
-                Text("Hello, world!")
-            }
-            Text("Hello, world!")
-            Text("Hello, world!")
+    class Task: Identifiable {    //一意のUUIDを含むため、classで宣言
+        let id = UUID()
+        var taskTitle: String
+        var checked: Bool
+    
+        init(taskTitle: String, checked: Bool){
+            self.taskTitle = taskTitle
+            self.checked = checked
         }
-        .padding()
+    }
+    
+    var tasks = [
+        Task(taskTitle: "散歩", checked: false),
+        Task(taskTitle: "宿題", checked: true),
+        Task(taskTitle: "昼寝", checked: true),
+    ]
+    
+    
+    
+    var body: some View {
+        NavigationStack{
+            List{
+                ForEach(tasks) {task in
+                    //                    HStack{
+                    if task.checked == true {
+                        Text("☑︎ \(task.taskTitle)")
+                    }
+                    else {
+                        Text("□ \(task.taskTitle)")
+                    }
+            }
+                Text("+").font(.title)
+        }
+        .padding().navigationTitle(Text("ToDo do!"))
+        .toolbar {
+            ToolbarItem {
+                Text("Delete")
+            }}
+            }
     }
 }
 
