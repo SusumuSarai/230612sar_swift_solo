cf084d229841cbf0ab78763460f21579de817d29
SusumuSarai <susumu_sarai@mail.toyota.co.jp>
2023-06-15 20:44:54 +0900

    update check box button

--- Todododo/Todododo/ContentView.swift
+++ Todododo/Todododo/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    class Task: Identifiable {    //一意のUUIDを含むため、classで宣言
+    struct Task: Identifiable, Equatable {    //一意のUUIDを含むため、classで宣言、後のエラーでstructに変更
         let id = UUID()
         var taskTitle: String
         var checked: Bool
@@ -19,9 +19,9 @@ struct ContentView: View {
         }
     }
     
-    var tasks = [
-        Task(taskTitle: "散歩", checked: false),
-        Task(taskTitle: "宿題", checked: true),
+    @State var tasks = [
+        Task(taskTitle: "散歩", checked: true),
+        Task(taskTitle: "宿題", checked: false),
         Task(taskTitle: "昼寝", checked: true),
     ]
     
@@ -31,13 +31,23 @@ struct ContentView: View {
         NavigationStack{
             List{
                 ForEach(tasks) {task in
-                    //                    HStack{
-                    if task.checked == true {
-                        Text("☑︎ \(task.taskTitle)")
-                    }
-                    else {
-                        Text("□ \(task.taskTitle)")
-                    }
+                    Button(action: {
+                        guard let index = self.tasks.firstIndex(of: task) else {
+                            return
+                        }
+                        self.tasks[index].checked.toggle()
+                    })
+
+                        {
+                            if task.checked {
+                                HStack{Text("☑︎")
+                                    Text(task.taskTitle).strikethrough().fontWeight(.ultraLight)}
+                            }
+                            else {
+                                HStack{Text("□")
+                                    Text(task.taskTitle)}
+//                                Text("□ \(task.taskTitle)")
+                            }}
             }
                 Text("+").font(.title)
         }
