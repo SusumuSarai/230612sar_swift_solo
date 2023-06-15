7ccf4370a0d2c4114157bcfeec53553b10fb3b69
SusumuSarai <susumu_sarai@mail.toyota.co.jp>
2023-06-15 14:40:57 +0900

    the original project had gone, so make new one.

---
+++ Todododo/Todododo/TodododoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodododoApp.swift
+//  Todododo
+//
+//  Created by user on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct TodododoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
